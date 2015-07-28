cadb8865f89648dccdc4751c0c6ee1ba9c9b6db2
Amy Tang <amytangcodes@gmail.com>
2015-07-28 10:11:07 -0400

    Error validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,10 @@
 class ArticlesController < ApplicationController
-  def index
-  	@articles = Article.new
-  end
-
-  def show
-  	@article = Article.find(params[:id])
-  end
-
   def new
+  	@article = Article.new
+  end
+
+  def edit 
+  	@articles = Article.find(params[:id])
   end
 
   def create
@@ -18,11 +15,26 @@ class ArticlesController < ApplicationController
   	else 
   		render 'new'
   	end		
-  	# @article.save
-  	# redirect_to @article
-  	# render plain: params[:article].inspect
   end
 
+  def show
+  	@article = Article.find(params[:id])
+  end
+
+  def index
+  	@articles = Article.all
+  end
+
+  def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+          redirect_to @article
+      else
+          render 'edit'
+      end
+  end
+   
   private
   	def article_params 
   		params.require(:article).permit(:title, :text)
